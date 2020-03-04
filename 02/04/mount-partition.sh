e5597b19043487d29619deafdd1e6ed950cfffbd
Bruno Bernardo <Bigsby@users.noreply.github.com>
2020-03-04 09:49:14 +0000

    Update mount-partition.sh

--- 02/04/mount-partition.sh
+++ 02/04/mount-partition.sh
@@ -9,5 +9,5 @@ end
 
 sudo mkfs -v -t ext4 /dev/sdb1
 sudo mkdir -pv /mnt/lfs
-sudo echo -e "\n/dev/sdb1\t/mnt/lfs\text4\tdefaults\t0\t0\n" >> /etc/fstab
+sudo echo -e "\n/dev/sdb1\t/mnt/lfs\text4\tdefaults\t0\t0\n" | sudo tee /etc/fstab
 sudo mount -a
