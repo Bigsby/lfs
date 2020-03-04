30bbb2f5b254007f1f43c4393056ba4cb8912686
Bruno Bernardo <Bigsby@users.noreply.github.com>
2020-03-04 10:12:46 +0000

    Update packages.sh

--- 03/01/packages.sh
+++ 03/01/packages.sh
@@ -1,8 +1,8 @@
 sudo mkdir -v $LFS/sources
 sudo chmod -v a+wt $LFS/sources
 wget http://www.linuxfromscratch.org/lfs/view/stable/wget-list
-http://www.linuxfromscratch.org/lfs/view/stable/md5sums
+wget --directory-prefix=$LFS/sources http://www.linuxfromscratch.org/lfs/view/stable/md5sums
 wget --input-file=wget-list --continue --directory-prefix=$LFS/sources
 pushd $LFS/sources
-md5sum -c ~/md5sums
+md5sum -c md5sums
 popd
